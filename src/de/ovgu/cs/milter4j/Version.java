337bcd287fbf5e1eb76b2a9d10fd71f2397e2798
Jens Elkner <jel+milter4j-src@linofee.org>
2007-11-23 02:54:45 +0000

    prefix build number with b

--- src/de/ovgu/cs/milter4j/Version.java
+++ src/de/ovgu/cs/milter4j/Version.java
@@ -25,7 +25,7 @@ public class Version {
 	private static final String yearEnd = "@year.end@";
 	private static final String vendorName = "@vendor.name@";
 	private static final String vendorURL = "@vendor.url@";
-	private static final String buildNumber = "@build.number@";
+	private static final String buildNumber = "b@build.number@";
 	
 	/**
 	 * Get the offical name of this product
