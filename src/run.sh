5ed6b53d6936b2f0c9ae20d6d769cfe2486b0898
Jens Elkner <jel+milter4j-src@linofee.org>
2026-04-27 07:57:14 +0200

    Simplify/unify the start/stop script.

--- src/run.sh
+++ src/run.sh
@@ -1,113 +1,130 @@
-#!/bin/ksh
+#!/bin/ksh93
 
-JAVA_HOME=${JAVA_HOME:-/local/apps/jdk}
+JAVA_HOME=${JAVA_HOME:-/opt/jdk}
+[[ ! -e ${JAVA_HOME} ]] && JAVA_HOME=/usr/jdk
 
-# additional flags for the java virtual machine
-JVM_FLAGS="-Djava.awt.headless=true -server"
+# Flags for the Java Virtual Machine to use on start as well as on shutdown
+JVM_FLAGS=( '-Djava.awt.headless=true' )
 
-if [ "$1" != "shutdown" ]; then
-	# Enable JMX to retrieve server information and statistics on demand
-	#	JMX clients connect to this TCP port to access the RMI registry
-	JVM_FLAGS="$JVM_FLAGS -Dcom.sun.management.jmxremote.port=12345"
-	#	TCP port used for JMX RMI client connections. If not set, a random
-	#	port is selected as needed, which can be difficult to handle properly
-	#	with firewalls.
-	#JVM_FLAGS="$JVM_FLAGS -Dcom.sun.management.jmxremote.rmi.port=12346"
-	#	Force JMX clients to use this IP or hostname/FQDN
-	#JVM_FLAGS="$JVM_FLAGS -Djava.rmi.server.hostname=127.0.0.1"
-	#	If a firewall is in place which allows only trusted clients within the
-	#	local network to connect, no authentication and no traffic encryption is
-	#	probably ok. If in doubts, inverse these settings.
-	JVM_FLAGS="$JVM_FLAGS -Dcom.sun.management.jmxremote.authenticate=false"
-	JVM_FLAGS="$JVM_FLAGS -Dcom.sun.management.jmxremote.ssl=false"
-	#	No dynamic class loading from remote codebases (security best practice).
-	JVM_FLAGS="$JVM_FLAGS -Djava.rmi.server.useCodebaseOnly=true"
-	#	For additional options, consult the JMX documentation or ask ChatGPT.
-	
-	# Just in case someone wants to attach a remote debugger:
-	#JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,address=45678,server=y,suspend=n"
-fi
+# Flags for the Java Virtual Machine to use on start, only.
+START_JVM_FLAGS=(
+	## JMX can be used to retrieve server information and statistics on demand.
+
+	# JMX clients connect to this TCP port to access the RMI registry.
+	'-Dcom.sun.management.jmxremote.port=12345'
+
+	# TCP port used for JMX RMI client connections. If not set, a random port
+	# is selected as needed, which can be difficult to handle with firewalls.
+	#'-Dcom.sun.management.jmxremote.rmi.port=12346'
+
+	# Force JMX clients to use this IP or hostname/FQDN
+	#'-Djava.rmi.server.hostname=127.0.0.1'
+
+	# If a firewall is in place which allows only trusted clients within the
+	# local network to connect, no authentication and no traffic encryption is
+	# probably ok. If in doubts, inverse these settings.
+	'-Dcom.sun.management.jmxremote.authenticate=false'
+	'-Dcom.sun.management.jmxremote.ssl=false'
+
+	# No dynamic class loading from remote codebases (security best practice).
+	'-Djava.rmi.server.useCodebaseOnly=true'
+
+	# For additional options, consult the JMX documentation or ask ChatGPT.
+
+	## Other flags
+	'-server'
+	'-Xmx256m'
+	#'-XX:+HeapDumpOnOutOfMemoryError'
+
+	# Just in case someone wants to attach a debugger from remote.
+	#'-agentlib:jdwp=transport=dt_socket,address=45678,server=y,suspend=n'
+)
+
+# Flags for the Java Virtual Machine to use on shutdown, only.
+STOP_JVM_FLAGS=(
+)
 
 #=========================================================================
 # no further changes required
 #=========================================================================
-START_CLASS=Server
+export LC_CTYPE=de_DE.UTF-8
 
-export LC_CTYPE=de_DE
+typeset -r VERSION='1.0' PRG=${.sh.file} PROG=${PRG##*/} SDIR=${PRG%/*} \
+	BASE_DIR=${SDIR%/*}
 
-Usage() {
-cat<<EOF
-Usage: ${0} [-h] [shutdown]
-
-    -h        print this help and exit
-    shutdown  stop the currently running instance
-EOF
+function showUsage {
+	getopts -a "${PROG}" "${ print ${USAGE} ; }" OPT --man
 }
 
-JAVA=`which java 2>/dev/null`
-if [ -n "$JAVA_HOME" ]; then
-	if [ -x ${JAVA_HOME}/bin/java ]; then
-		JAVA=${JAVA_HOME}/bin/java
-	fi
-fi
+USAGE="[-?${VERSION}"' ]
+[-copyright?Copyright (c) 2007-2026 Jens Elkner. All rights reserved.]
+[+NAME?'"${PROG}"' - simple script to start milter4j]
+[+DESCRIPTION?This script allows to start and stop the milter4j server. If the \bjava\b interpreter is not in your \bPATH\b add it or set \bJAVA_HOME\b to the directory containing \bbin/java\b.]
+[h:help?Print this help and exit.]
+[+?]
+[c:config]:[file?Start or stop the server with the given configuration file.]
+[k:kill?Shutdown the running server and exit. It uses the values from the related configuration to connect to the daemon and send it the signal to shutdown.]
+[L:log]:[file?Use the given \afile\a for logging configuration. Must be a logback XML configuration file.]
+\n\n[\ashutdown\a]
+'
 
-if [ ! -x "$JAVA" ]; then
-	echo "JVM not found. Set JAVA_HOME or PATH env variable!"
-	exit 1
-fi
+unset ARGS CFG START_CLASS ; typeset -a ARGS
+integer IS_START_STOP=1
+typeset -n FLAGS=START_JVM_FLAGS
 
-if [ -z "$JVM_FLAGS" ]; then
-	JVM_FLAGS=" "
-fi
-
-while getopts "h" option ; do
-	case "$option" in
-		h) Usage; exit 0 ;;
+while getopts "${USAGE}" OPT ; do
+	case "${OPT}" in
+		h) showUsage ; exit 0 ;;
+		c) CFG="${OPTARG}" ;;
+		k) IS_START_STOP=2 ;;
+		# add your own filters like this
+		#e) START_CLASS=de.ovgu.cs.jelmilter.HeloCheck IS_START_STOP=0 ;;
+		L) JVM_FLAGS+=( "-Dlogback.config-file=${OPTARG}" ) ;;
 	esac
-done	
+done
 X=$((OPTIND-1))
 shift $X
 
-# resolv links to base directory
-PRG=$0
-progname=`basename $0`
-while [ -h "$PRG" ] ; do
-	ls=`ls -ld "$PRG"`
-	link=`expr "$ls" : '.*-> \(.*\)$'`
-	if expr "$link" : '.*/.*' > /dev/null; then
-		PRG="$link"
-	else
-		PRG="`dirname $PRG`/$link"
+if (( IS_START_STOP )); then
+	START_CLASS=de.ovgu.cs.milter4j.Server
+	unset ARGS
+	[[ -n ${CFG} ]] && ARGS=( "${CFG}" )
+	if (( IS_START_STOP == 2 )); then
+		typeset -n FLAGS=STOP_JVM_FLAGS
+		ARGS+=( 'shutdown' )
 	fi
-done
-BASE_DIR=`dirname "$PRG"`/..
+else
+	for ((I=${#START_JVM_FLAGS}-1; I >= 0; I-- )); do
+		if [[ ${START_JVM_FLAGS[I]} =~ port= ]]; then
+			START_JVM_FLAGS[I]='-esa'
+		fi
+	done
+fi
 
-if [ ! -d "${BASE_DIR}/lib" ]; then
-	echo "Base directory $BASE_DIR/lib does not exist. Exiting"
+[[ -n ${JAVA_HOME} ]] && [[ -x ${JAVA_HOME}/bin/java ]] && \
+	JAVA=${JAVA_HOME}/bin/java
+[[ -z ${JAVA} || ! -x ${JAVA} ]] && JAVA=${ whence java ; }
+if [[ -z ${JAVA} || ! -x ${JAVA} ]]; then
+	print -u2 'JVM not found. Set JAVA_HOME or PATH env variable!'
+	exit 1
+fi
+
+if [[ ! -d ${BASE_DIR}/lib ]]; then
+	print -u2 "Base directory ${BASE_DIR}/lib does not exist. Exiting."
 	exit 2
 fi
 
 # add in the dependency .jar files
-DIRLIBS=${BASE_DIR}/lib/*.jar
-LOCAL_CLASSPATH="${BASE_DIR}/lib"
-for i in $DIRLIBS ; do
-    # if the directory is empty, then it will return the input string
-    # this is stupid, so case for it
-	if [ "$i" != "${DIRLIBS}" ] ; then
-		LOCAL_CLASSPATH="$i":$LOCAL_CLASSPATH
-	fi
+(( IS_START_STOP )) && X= || X=":${BASE_DIR}/lib/test"
+for F in ~(N)${BASE_DIR}/lib/*.jar ${BASE_DIR}/lib ; do
+	[[ ${F: -8:8} == '-src.jar' ]] && continue
+	X+=":$F"
 done
 
-if [ -n "$LOCAL_CLASSPATH" ]; then
-	LOCAL_CLASSPATH="-cp ${LOCAL_CLASSPATH}"
-fi
+[[ -d ${BASE_DIR}/lib/endorsed ]] && \
+	FLAGS+=( "-Djava.endorsed.dirs=${BASE_DIR}/lib/endorsed" )
+[[ -n $X  ]] && FLAGS+=( '-cp' "${X:1}" )
 
-if [ -d "${BASE_DIR}/lib/endorsed" ]; then
-	if [ -z "$JVM_FLAGS" ]; then
-		JVM_FLAGS="-Djava.endorsed.dirs=${BASE_DIR}/lib/endorsed"
-	else
-		JVM_FLAGS="$JVM_FLAGS -Djava.endorsed.dirs=${BASE_DIR}/lib/endorsed"
-	fi
-fi
+exec ${JAVA} "${JVM_FLAGS[@]}" "${FLAGS[@]}" ${START_CLASS} "${ARGS[@]}" "$@"
 
-exec $JAVA ${JVM_FLAGS} ${JAVA_OPTS} $LOCAL_CLASSPATH de.ovgu.cs.milter4j.$START_CLASS $@
+# vim: ts=4 sw=4 filetype=sh
