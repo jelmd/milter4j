a4081d61d8e21d34842d55e1b2fbd94036267034
Jens Elkner <jel+milter4j-src@linofee.org>
2026-05-04 20:03:23 +0200

    Change license to EPL 2.0

--- src/de/ovgu/cs/milter4j/reply/Type.java
+++ src/de/ovgu/cs/milter4j/reply/Type.java
@@ -1,11 +1,12 @@
 /**
- * $Id$ 
- * 
  * Copyright (c) 2005-2007 Jens Elkner.
  * All Rights Reserved.
  *
- * This software is the proprietary information of Jens Elkner.
- * Use is subject to license terms.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  */
 package de.ovgu.cs.milter4j.reply;
 
